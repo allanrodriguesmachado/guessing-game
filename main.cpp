c1fa4d9db89fb14e272aecfd41dcd8a3c6870afa
Allan Rodrigues <allan.rodrigues14@hotmail.com>
2023-08-23 16:55:19 -0300

    Update Project

--- main.cpp
+++ main.cpp
@@ -5,40 +5,45 @@
 
 using namespace std;
 
+bool test(bool verify) {
+    const int NUMBER_SECRET = rand();
+    double result = 1000.0;
+
+    while (verify) {
+        int kickNumber = 0;
+
+        cout << "Digite um numero: " << kickNumber << endl;
+        cin >> kickNumber;
+
+        double perda = abs(kickNumber - NUMBER_SECRET) / 2.0;
+        result = result - perda;
+
+        if (kickNumber == NUMBER_SECRET) {
+            cout << "Parabens, sua pontuacao final foi " << result << endl;
+            return verify = false;
+        }
+
+        if (kickNumber < NUMBER_SECRET) {
+            cout << "Nao foi dessa vez, Tentar novamente valor menor" << endl;
+        }
+
+        if (kickNumber > NUMBER_SECRET) {
+            cout << "Nao foi dessa vez, Tentar novamente  valor maior" << endl;
+        }
+
+        cout << "Pontuacao atual " << result << endl;
+    }
+
+}
+
 int main() {
     cout << "************************************" << endl;
     cout << "* Bem-vindos ao jogo da advinhacao *" << endl;
     cout << "************************************" << endl;
 
-    const int NUMBER_SECRET = rand();
     bool verify = true;
-    double result = 1000.0;
 
-    while (verify) {
-        int kickNumber;
-        cin >> kickNumber;
-      
-        double perda = abs(kickNumber - NUMBER_SECRET) / 2.0;
-        result = result - perda;
+    test(verify);
 
-        if (kickNumber == NUMBER_SECRET) {
-            cout << "Parabens" << result << endl;
-            return verify = false;
-        }
-
-        if (kickNumber != NUMBER_SECRET) {
-            
-            cout << "Nao foi dessa vez, Tentar novamente"  <<  endl;
-        }
-
-        if (kickNumber < NUMBER_SECRET) {
-            cout << "Nao foi dessa vez, Tentar novamente valor menor"  << endl;
-        }
-        
-        if (kickNumber > NUMBER_SECRET) {
-            cout << "Nao foi dessa vez, Tentar novamente valor menor valor maior"  <<  endl;
-        }
-    }
-
-      cout << "Sua pontuação foi de"  <<  result << endl;
+    return 0;
 }
