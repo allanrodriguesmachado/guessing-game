f4760e8b4ff1a284dfd9417c9e844da780b4ccb0
Allan Rodrigues <allan.rodrigues14@hotmail.com>
2023-08-25 11:28:56 -0300

    Refactoring of the project, adjusting functions and expressions. Final project completed 100% functional

--- main.cpp
+++ main.cpp
@@ -4,72 +4,90 @@
 
 using namespace std;
 
-bool validateGame(bool verify) {
-    srand(time(NULL));
-    const int NUMBER_SECRET = rand() % 100;
-    double result = 1000.0;
-    int quantidade = 0;
+double totalPoints = 1000.0;
+int gameOdds = 0;
 
-    char dificulti;
-    cin >> dificulti;
-
-    int tentativasA;
-
-    if (dificulti == 'f') {
-        tentativasA = 12;
-    }
-
-    if (dificulti == 'm') {
-        tentativasA = 6;
-    }
-
-    if (dificulti == 'd') {
-        tentativasA = 3;
-    }
-
-    for (int i = 1; i <= tentativasA; i++) {
-        int kickNumber;
-        cout << "Digite um numero: " << endl;
-        cin >> kickNumber;
-
-        double perda = abs(kickNumber - NUMBER_SECRET) / 2.0;
-        result = result - perda;
-
-        cout << "Quantidades " << i << endl;
-
-        cout.precision(2);
-        cout << fixed;
-        if (kickNumber == NUMBER_SECRET) {
-            cout << "Parabens" << endl;
-            break;
-        }
-
-        if (kickNumber < NUMBER_SECRET) {
-            cout << "Nao foi dessa vez, Tentar novamente valor menor" << endl;
-        }
-
-        if (kickNumber > NUMBER_SECRET) {
-            cout << "Nao foi dessa vez, Tentar novamente  valor maior" << endl;
-        }
-        cout << "******************************************" << endl;
-        cout << "Nao foi dessa vez " << endl;
-        cout << "Resultado final " << result << endl;
-    }
-
-    return true;
-}
-
-int main() {
+void header() {
     cout << "************************************" << endl;
     cout << "* Bem-vindos ao jogo da advinhacao *" << endl;
     cout << "************************************" << endl;
 
     cout << "Escolha o nivel de dificuldade" << endl;
     cout << "Facil: F, Medio: M ou Dificil: D" << endl;
+}
 
-    bool verify = true;
 
-    validateGame(verify);
+void footer() {
+    cout << "******************************************" << endl;
+    cout << "Nao foi dessa vez " << endl;
+    cout << "Resultado final " << totalPoints << endl;
+}
+
+int secretNumber() {
+    srand(time(nullptr));
+    const int NUMBER_SECRET = rand() % 100;
+    return NUMBER_SECRET;
+}
+
+void gameLevel() {
+    char selectLevel;
+    cin >> selectLevel;
+    if (selectLevel == 'f') {
+        gameOdds = 10;
+    }
+
+    if (selectLevel == 'm') {
+        gameOdds = 4;
+    }
+
+    if (selectLevel == 'd') {
+        gameOdds = 3;
+    }
+}
+
+double calculatePointLoss(int kickNumber, int numero) {
+    double calculatePointLoss = abs(kickNumber - numero) / 2.0;
+    return totalPoints = totalPoints - calculatePointLoss;
+}
+
+void gameValidation() {
+    for (int i = 1; i <= gameOdds; i++) {
+        int kickNumber;
+        cout << "Digite um numero de 0 a 100: " << endl;
+        cin >> kickNumber;
+
+        calculatePointLoss(kickNumber, secretNumber());
+
+        cout << "Quantidades " << i << endl;
+        cout.precision(2);
+        cout << fixed;
+
+        if (kickNumber > 100) {
+            cout << "Ops! Numero muito! Tente novamente" << endl;
+        }
+
+        if (kickNumber == secretNumber()) {
+            cout << "Parabens" << endl;
+            break;
+        }
+
+        if (kickNumber < secretNumber()) {
+            cout << "Nao foi dessa vez, Tentar novamente valor menor" << endl;
+        }
+
+        if (kickNumber > secretNumber()) {
+            cout << "Nao foi dessa vez, Tentar novamente  valor maior" << endl;
+        }
+    }
+}
+
+int main() {
+    header();
+
+    gameLevel();
+    gameValidation();
+
+    footer();
 
     return 0;
 }
